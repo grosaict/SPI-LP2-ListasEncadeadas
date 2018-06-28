d64bd74064985bfde5938d8438bdb5b400d69b28
grosaict <grosa.ict@gmail.com>
2018-06-28 17:27:29 -0300

    Alterações na classe Vetor

--- src/Vetor.java
+++ src/Vetor.java
@@ -9,13 +9,16 @@ public class Vetor<T> {
 
 	public No searchSmallerNo(T data) {
 		No smallerNo = null, auxNo = this.head;
-		No[] index;
-		int size = 0, increase = 100, start = 0, foward = 10, rewind = 5, max = foward, min;
+		No[] index = null;
+		int size = 0, increase = 100, start = 0, foward = 10, rewind = 5, max = foward, top = foward, min = 0;
 		
-		index = madeArray(null, size, increase);
+		index = madeArray(index, size, increase);
 		size = size + increase;
 		
 		while (smallerNo == null) {
+			if (max > top){
+				max = top;
+			}
 			for (int i=start; i<max; i++){
 				if (i >= size){
 					index = madeArray(index, size, increase);		// aumenta o vetor se necessário
@@ -26,71 +29,87 @@ public class Vetor<T> {
 				if (auxNo != this.tail){
 					auxNo = auxNo.getNext();
 				} else {
-					max = i;
+					top = max = i;
 				}
 			}
-/* debug */	System.out.println(data+"<="+auxNo.getData());
-			if (((String) data).compareTo((String) auxNo.getData()) <= 0) {							// se ainda é <= que o valor atual ...
-/* debug */		System.out.println(data+">="+auxNo.getPrev().getData());
-				if (((String) data).compareTo((String) auxNo.getPrev().getData()) >= 0) {			// ... testa se o anterior é >= (goal) ...
-					smallerNo = auxNo.getPrev();
-				} else {																			// ... senão começa a rebobinar
-/* debug */			System.out.println(data+"<"+auxNo.getPrev().getData());
-					while (((String) data).compareTo((String) auxNo.getPrev().getData()) < 0 && smallerNo == null) {
-/* debug */				System.out.println(data+"<"+auxNo.getPrev().getData());
-						min = max - rewind;
-						if (min <= 0){
-							min = 1;
+			if (isBetween(data, auxNo.getPrev().getData(), auxNo.getData())) {			// Se está entre ...
+				smallerNo = auxNo.getPrev();											// então GOAL!
+			} else {
+				while (isTooAdvanced(data, auxNo.getPrev().getData(), auxNo.getData()) && smallerNo == null) {	// Se passou do ponto ...
+					min = max - rewind;																			// ... então retrocede ...
+					if (min <= 0){
+						min = 1;
+					}
+/* debug */			System.out.println((min)+"R"+auxNo.getData());
+					if (isBetween(data, auxNo.getPrev().getData(), auxNo.getData())) {	// Se está entre ...
+						smallerNo = auxNo;												// ... então GOAL!
+					} else {																
+						if (!isFarBehind(data, auxNo.getPrev().getData(), auxNo.getData())) {	// Se não passou do ponto rebobina mais um pouco.
+							min = min - rewind;
+							if (min <= 0){
+								min = 1;
+							}
+							auxNo = index[min];
 						}
-/* debug */				System.out.println((min)+"R"+auxNo.getData());
-	/* debug */			System.out.println(data+">="+auxNo.getPrev().getData());
-						if (((String) data).compareTo((String) auxNo.getPrev().getData()) >= 0) {	// Se é >= que o anterior ...
-	/* debug */				System.out.println(data+"<="+auxNo.getData());
-							if (((String) data).compareTo((String) auxNo.getData()) <= 0) {			// ... testa se o anterior é <= (goal) ...
-								smallerNo = auxNo;
-							} else {																// ... senão rebobina mais um pouco
-								rewind = rewind /2;
-								if (rewind <= 0){
-									rewind = 1;
-								}
-							}
-						} else {
-							if ((max - rewind) <= 0){
-								start = 1;
-							} else {
-								start = max - rewind;
-							}
-							foward = rewind /2;
-							if (foward <= 0){
-								foward = 1;
-							}
-							rewind = foward /2;
-							if (rewind <= 0){
-								rewind = 1;
-							}
-							max = start + foward;
-						}
-						auxNo = index[min];
 					}
 				}
-			} else {
-				start = max;
-				max = max + foward;
+				if (min <= 0){														// Avança mais um pouco ...
+					start = 1;
+				} else {
+					start = min;													// ... a partir de onde parou ...
+				}
+				foward = rewind / 2;												// ... reduzindo o avanço pela metade ...
+				if (foward <= 0){
+					foward = 1;
+				}
+				rewind = foward / 2;												// ... e o retorno também ...
+				if (rewind <= 0){
+					rewind = 1;
+				}
+				max = start + foward;
 			}
 		}
 		return smallerNo;
 	}
 	
-	private No[] madeArray(No[] index, int size, int increase) {		// cria ou aumenta um vetor dinâmico de No's
-		if (size == 0){
-			No[] newIndex = new No[size + increase];
-			return newIndex;
+	private boolean isBetween (T data, Object anterior, Object seguinte){
+/* debug */	System.out.println(anterior+" <= "+data+" <= "+seguinte);
+		if (((String) data).compareTo((String) anterior) >= 0 &&
+			((String) data).compareTo((String) seguinte) <= 0) {
+			return true;
 		} else {
-			No[] newIndex = new No[size + increase];
+			return false;
+		}
+	}
+	
+	private boolean isTooAdvanced (T data, Object anterior, Object seguinte){
+/* debug */	System.out.println(anterior+" > "+data+" < "+seguinte);
+		if (((String) data).compareTo((String) anterior) < 0 &&
+			((String) data).compareTo((String) seguinte) < 0) {
+				return true;
+			} else {
+				return false;
+			}
+	}
+	
+	private boolean isFarBehind (T data, Object anterior, Object seguinte){
+/* debug */	System.out.println(anterior+" < "+data+" > "+seguinte);
+		if (((String) data).compareTo((String) anterior) > 0 &&
+			((String) data).compareTo((String) seguinte) > 0) {
+				return true;
+			} else {
+				return false;
+			}
+	}
+
+
+	private No[] madeArray(No[] index, int size, int increase) {		// cria ou aumenta um vetor dinâmico de No's
+		No[] newIndex = new No[size + increase];
+		if (size != 0){
 			for (int i=0; i < size; i++){
 				newIndex[i] = index[i];
 			}
-			return newIndex;
 		}
+		return newIndex;
 	}
 }
